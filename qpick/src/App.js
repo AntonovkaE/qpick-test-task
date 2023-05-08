a6aec1c00c603439f7e60ba914364ec028acdbef
Elvira <sabirova.elvira.gam@gmail.com>
2023-05-08 06:55:49 +0500

    feat: добавить стили для Header

--- qpick/src/App.js
+++ qpick/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {headphones} from './utils/headphones';
 import Catalog from './components/catalog/Catalog';
 import { useState } from 'react';
+import Header from './components/header/Header';
 
 function App() {
   const [cart, setCart] = useState([])
@@ -11,6 +12,7 @@ function App() {
   }
   return (
     <div className="App">
+      <Header/>
       <Catalog headphones={headphones} addInBasket={addInBasket}/>
     </div>
   );
--- qpick/src/components/header/Header.js
+++ qpick/src/components/header/Header.js
@@ -6,17 +6,20 @@ import cartIcon from '../../assets/img/cart.svg'
 function Header({cartCounter = 2, favoriteCounter=2}) {
   return (
     <header className="header">
-      <a href='#'>
+      <a className='header__logo link' href='#'>
         <h1 className="header__logo">QPICK</h1>
       </a>
-      <a href="" className="header__favorite-link">
-        <img src={heartIcon} alt="избранное" className="header__favorite"/>
-        <div className="header__favorite-counter">{favoriteCounter}</div>
-      </a>
-      <a href="" className="header__cart-link">
-        <img src={cartIcon} alt="Корзина" className="header__cart"/>
-        <div className="header__cart-counter">{cartCounter}</div>
-      </a>
+      <nav className="header__nav">
+        <a href="#" className="header__favorite-link">
+          <img src={heartIcon} alt="избранное" className="header__favorite"/>
+          <div className="header__counter header__counter_favorite">{favoriteCounter}</div>
+        </a>
+        <a href="#" className="header__cart-link">
+          <img src={cartIcon} alt="Корзина" className="header__cart"/>
+          <div className="header__counter header__counter_cart">{cartCounter}</div>
+        </a>
+      </nav>
+
     </header>
   );
 }
